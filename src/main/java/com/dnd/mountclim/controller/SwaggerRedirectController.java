575f1fbd10a1ab0fa05808473df621bf222a9cc4
dkswjdrbs123 <dkswjdrbs12@naver.com>
2022-07-13 09:16:26 +0900

    feat: Add swagger redirect class

---
+++ src/main/java/com/dnd/mountclim/controller/SwaggerRedirectController.java
@@ -0,0 +1,16 @@
+package com.dnd.mountclim.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import springfox.documentation.annotations.ApiIgnore;
+
+@Controller
+@RequestMapping("/api/doc")
+@ApiIgnore
+public class SwaggerRedirectController {
+    @GetMapping
+    public String swaggerUrlRedirect(){
+        return "redirect:/swagger-ui/index.html";
+    }
+}
