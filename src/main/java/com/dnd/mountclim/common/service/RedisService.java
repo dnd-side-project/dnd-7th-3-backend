ab8fbc9c2eafe3b87ccd2ec62c5fe105eb311f40
Anjeongkyun <dkswjdrbs12@naver.com>
2022-07-19 22:39:49 +0900

    feat: swagger & redis class 생성

---
+++ src/main/java/com/dnd/mountclim/common/service/RedisService.java
@@ -0,0 +1,11 @@
+package com.dnd.mountclim.common.service;
+
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Service
+public class RedisService {
+
+}
