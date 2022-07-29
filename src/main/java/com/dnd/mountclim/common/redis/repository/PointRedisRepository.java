6d59ddd1e0e75351feb2810360343f850501325b
haeyong Hahn <yong80211@gmail.com>
2022-07-29 09:10:27 +0900

    fix:로그인설정 제거

--- src/main/java/com/dnd/mountclim/common/redis/repository/PointRedisRepository.java
+++
@@ -1,8 +0,0 @@
-package com.dnd.mountclim.common.redis.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.dnd.mountclim.common.redis.domain.Point;
-
-public interface PointRedisRepository extends CrudRepository<Point, String> {
-}
